3b68ee59c8d3bdc2ab972252fe18bb9cd5019e3a
Dival Lucas <lusca404@gmail.com>
2024-10-23 11:02:58 -0300

    feature login & register

--- ondeficar/urls.py
+++ ondeficar/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import login
+from .views import HomeView, LoginView, RegisterView
 
 urlpatterns = [
-    path('login/', login, name='login'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('', HomeView.as_view(), name='home'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
 
--- ondeficar/views.py
+++ ondeficar/views.py
@@ -1,19 +1,31 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView, FormView
+from .models import Hotel, Lugar, Locatario, Hospede
+from .forms import UserRegisterForm, HospedeForm, LocatarioForm, HotelForm, LugarForm
 
-def login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('página_inicial') 
-    else:
-        form = AuthenticationForm()
-    
-    return render(request, 'login.html', {'form': form})
+class HotelListView(ListView):
+    model = Hotel
+    template_name = 'hotel.html'
 
+class HomeView(ListView):
+    model = Lugar
+    template_name = 'index.html'
+
+class LoginView(FormView):
+    form_class = AuthenticationForm
+    template_name = 'login.html'
+    success_url = '/'
+
+    def form_valid(self, form):
+        usuario = form.get_user()
+        login(self.request, usuario)
+        return super().form_valid(form)
+
+class RegisterView(CreateView):
+    form_class = UserRegisterForm
+    template_name = 'register.html'
+    success_url = '/login'
