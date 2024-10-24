686ff57ce7e06de536955bf6ec42a65a554b51a5
Dival Lucas <lusca404@gmail.com>
2024-10-23 23:15:23 -0300

    fixing bugs

--- ondeficar/models.py
+++ ondeficar/models.py
@@ -19,12 +19,13 @@ class Lugar(models.Model):
         ('hotel', 'Hotel'),
     ]
 
+    nome = models.CharField(max_length=255)  # Nome do lugar
     preco = models.DecimalField(max_digits=10, decimal_places=2)  # Preço do lugar
     localizacao = models.CharField(max_length=255)  # Localização do lugar
     categoria = models.CharField(max_length=10, choices=CATEGORIA_CHOICES)  # Categoria (Navio, Casa, Hotel)
     descricao = models.TextField()  # Descrição do lugar
     link = models.URLField()  # Link para o lugar
-    classificacao = models.DecimalField(max_digits=3, decimal_places=2)  # Classificação com duas casas decimais
+    classificacao = models.IntegerField()
     locatario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='lugares')  # Locatário que cadastrou o lugar
     foto = models.ImageField(upload_to='fotos_lugares/', blank=True, null=True)  # Campo para foto do lugar
 
